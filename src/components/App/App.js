1c6340f9bf0b0ce68d25e9fd60a72977c49c70d0
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-07-28 14:13:55 +0300

    Moved business information to the container component (App); Passed information from parent components (App) to child components (BusinessList, Business)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,33 @@
 import "./App.css";
 import Business from "../Business/Business";
-import React from 'react'
-import BusinessList from '../BusinessList/BusinessList'
-import SearchBar from '../SearchBar/Searchbar'
+import React from "react";
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/Searchbar";
 
+const business = {
+  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar/>
-      <BusinessList/>
-    </div>
-    )
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses}/>
+      </div>
+    );
   }
-};
-
+}
 
 export default App;
